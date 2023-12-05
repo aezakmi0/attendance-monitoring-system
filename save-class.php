3420a252c0a8e6c9dfd33b613675fa8e4220413c
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-06 01:11:33 +0800

    added php file

---
+++ save-class.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_attendance";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $classCode = $_POST["class_code"];
+    $className = $_POST["class_name"];
+    $room = $_POST["room"];
+    $startTime = $_POST["time_start"];
+    $endTime = $_POST["time_end"];
+    $days = implode(",", $_POST["days"]); // Assuming you store days as comma-separated values
+
+    // SQL query to insert data into the database
+    $sql = "INSERT INTO tb_class (class_code, class_name, room, time_start, time_end, day) VALUES ('$classCode', '$className', '$room', '$startTime', '$endTime', '$days')";
+
+    // Execute the query
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
