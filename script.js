13786f535aba2272074752399830065cdbdba155
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-07 22:45:38 +0800

    correct javascript for drag and drop

--- script.js
+++ script.js
@@ -65,18 +65,28 @@ function saveSelectedDays() {
     container.addEventListener('dragover', e => {
       e.preventDefault()
       const afterElement = getDragAfterElement(container, e.clientY)
+      console.log(afterElement)
       const draggable = document.querySelector('.dragging')
-      container.appendChild(draggable)
+      if(afterElement == null){
+        container.appendChild(draggable)
+      } else{
+        container.insertBefore(draggable, afterElement)
+      }
     })
   })
 
   function getDragAfterElement(container, y){
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    const draggableElements = [...container.querySelectorAll('.seatplan-seat:not(.dragging)')]
     
-    draggableElements.reduce((closest, child) => {
+    return draggableElements.reduce((closest, child) => {
       const box = child.getBoundingClientRect()
-      console.log(box);
-    }, { offset: Number.POSITIVE_INFINITY })
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+    } else {
+        return closest;
+    }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
   }
 
   // 16:40 https://www.youtube.com/watch?v=jfYWwQrtzzY
