d8baa29f09d313b85ccdb3cb50eed2c1a9541232
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-07 20:47:36 +0800

    followed a tutorial about dragging
    https://www.youtube.com/watch?v=jfYWwQrtzzY

--- script.js
+++ script.js
@@ -43,8 +43,40 @@ function saveSelectedDays() {
       },
       error: function(error) {
         console.error('Error saving data:', error);
-      }
+      } 
     });
   }
   
-  const sortable = Sortable.create(document.getElementById('sortable-container'));
+  // const sortable = Sortable.create(document.getElementById('sortable-container'));
+
+  const draggables = document.querySelectorAll('.seatplan-seat');
+  const containers = document.querySelectorAll('.seatplan-container');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () =>{
+      draggable.classList.add('dragging')
+    })
+    draggable.addEventListener('dragend', () => {
+      draggable.classList.remove('dragging')
+    })
+  })
+
+  containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+      e.preventDefault()
+      const afterElement = getDragAfterElement(container, e.clientY)
+      const draggable = document.querySelector('.dragging')
+      container.appendChild(draggable)
+    })
+  })
+
+  function getDragAfterElement(container, y){
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    
+    draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      console.log(box);
+    }, { offset: Number.POSITIVE_INFINITY })
+  }
+
+  // 16:40 https://www.youtube.com/watch?v=jfYWwQrtzzY
