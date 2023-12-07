c32c043220d866708936d5b7b6d2313d66cc4dc6
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-08 00:50:25 +0800

    try snapping drop

--- script.js
+++ script.js
@@ -49,44 +49,21 @@ function saveSelectedDays() {
   
   // const sortable = Sortable.create(document.getElementById('sortable-container'));
 
-  const draggables = document.querySelectorAll('.seatplan-seat');
-  const containers = document.querySelectorAll('.seatplan-container');
+  $(document).ready(function() {
+    // Make seatplan-seat elements draggable
+    $('.seatplan-seat').draggable({
+      snap: '.seatplan-seat-behind', // Snap to other seatplan-seat elements
+      snapMode: 'inner', // Snap to the outer edges of other elements
+      snapTolerance: 85, // Tolerance for snapping (adjust as needed)
+      stack: '.seatplan-seat', // Stack elements on top of each other when dragged
+      containment: '.seatplan-main-container' // Restrict dragging within the .container
+    });
 
-  draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () =>{
-      draggable.classList.add('dragging')
-    })
-    draggable.addEventListener('dragend', () => {
-      draggable.classList.remove('dragging')
-    })
-  })
+    // Make the container droppable
+    $('.container').droppable();
 
-  containers.forEach(container => {
-    container.addEventListener('dragover', e => {
-      e.preventDefault()
-      const afterElement = getDragAfterElement(container, e.clientY)
-      console.log(afterElement)
-      const draggable = document.querySelector('.dragging')
-      if(afterElement == null){
-        container.appendChild(draggable)
-      } else{
-        container.insertBefore(draggable, afterElement)
-      }
-    })
-  })
-
-  function getDragAfterElement(container, y){
-    const draggableElements = [...container.querySelectorAll('.seatplan-seat:not(.dragging)')]
-    
-    return draggableElements.reduce((closest, child) => {
-      const box = child.getBoundingClientRect()
-      const offset = y - box.top - box.height / 2
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child };
-    } else {
-        return closest;
-    }
-    }, { offset: Number.NEGATIVE_INFINITY }).element
-  }
-
-  // 16:40 https://www.youtube.com/watch?v=jfYWwQrtzzY
+    // Handle the drop event
+    $('.seatplan-seat').on('dragstop', function(event, ui) {
+      // You can handle additional logic here if needed
+    });
+  });
