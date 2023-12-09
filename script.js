3929ee3fc2f9148915bc1877415a6a0fa09fc31a
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-09 21:47:28 +0800

    transferred the js into html

--- script.js
+++ script.js
@@ -13,39 +13,6 @@ function includeHTML() {
     xhttp.send();
 }
 
-// Call the includeHTML function
-includeHTML();
-
-let selectedDays = [];
-
-function toggleDay(day) {
-  const index = selectedDays.indexOf(day);
-
-  if (index === -1) {
-    // Add the day to the array if it's not already present
-    selectedDays.push(day);
-  } else {
-    // Remove the day from the array if it's already present
-    selectedDays.splice(index, 1);
-  }
-}
-
-// Assuming you have a function to make an AJAX request to your server
-function saveSelectedDays() {
-    // Make an AJAX request to your server with the selectedDays data
-    // Example using jQuery
-    $.ajax({
-      type: 'POST',
-      url: 'save-class.php',
-      data: { days: selectedDays },
-      success: function(response) {
-        console.log('Data saved successfully');
-      },
-      error: function(error) {
-        console.error('Error saving data:', error);
-      } 
-    });
-  }
   
   // const sortable = Sortable.create(document.getElementById('sortable-container'));
 
