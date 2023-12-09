c5aac26ea4b7132dbfd5c6c6b0727155b20081eb
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-10 01:24:37 +0800

    updated the navbar display javascript

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-function includeHTML() {
-    var element = document.getElementById('navbar-container');
-    var xhttp = new XMLHttpRequest();
+// Include navbar
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("navbar.html")
+      .then(response => response.text())
+      .then(data => {
+          document.body.insertAdjacentHTML("afterbegin", data);
+      });
+});
 
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            element.innerHTML = this.responseText;
-        }
-    };
-
-    // Specify the path to your navbar.html file
-    xhttp.open('GET', 'navbar.html', true);
-    xhttp.send();
-}
-
-  
-  // const sortable = Sortable.create(document.getElementById('sortable-container'));
-
+// For edit-class drag and drop
   var grid1 = new Muuri('.grid-1', {
     dragEnabled: true,
     dragContainer: document.body,
