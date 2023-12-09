b5cc1b957fdd72145628dc6712900f2881d1cc3f
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-09 22:37:42 +0800

    correctly added days to db

--- save-class.php
+++ save-class.php
@@ -1,19 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_attendance";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
-
-<?php
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
@@ -23,12 +8,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $time_start = $_POST["time_start"];
     $time_end = $_POST["time_end"];
 
-    // Check if "days" key exists and is an array
-    $days = isset($_POST["day"]) && is_array($_POST["day"]) ? implode(",", $_POST["day"]) : '';
+    // Check if "day" key exists and is an array
+    $selected_days = isset($_POST["day"]) && is_array($_POST["day"]) ? implode("", $_POST["day"]) : '';
 
     // SQL query to insert data into the database
     $sql = "INSERT INTO tb_class (class_code, class_name, room, time_start, time_end, day)
-            VALUES ('$class_code', '$class_name', '$room', '$time_start', '$time_end', '$days')";
+            VALUES ('$class_code', '$class_name', '$room', '$time_start', '$time_end', '$selected_days')";
+
+    // Create connection (replace these values with your actual database credentials)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_attendance";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
     // Execute the query
     if ($conn->query($sql) === TRUE) {
