b146335915b25c5aaa0bc56280d4c162c20d7feb
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-21 01:33:48 +0800

    updated the table layout

--- attendance-records.php
+++ attendance-records.php
@@ -31,7 +31,10 @@ $classId = isset($_GET['id']) ? $_GET['id'] : null;
         .table td.fit, 
         .table th.fit {
             white-space: nowrap;
-            width: 5%;
+            width: 10%;
+        }
+        .status-warning{
+            background-color: #ffb0b7 !important;
         }
     </style>
 </head>
@@ -44,33 +47,52 @@ $classId = isset($_GET['id']) ? $_GET['id'] : null;
     </div>
     <hr/>
 
-    <div class="table-responsive">
-        <table class="table table-sm table-striped">
-        <thead>
-            <tr>
-                <th>ID Number</th>
-                <th>Student Name</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>10/4/23</th>
-                <th>Total</th>
+    <div class="table-responsive text-center">
+        <!-- <table class="table table-sm table-striped"> -->
+        <table class="table table-hover table-bordered">
+        <thead class="table-light">
+            <tr class="align-middle">
+                <th rowspan="2">ID Number</th>
+                <th rowspan="2">Student Name</th>
+                <!-- Months -->
+                <th colspan="9" class="th-months">November 2023</th>  <!-- colspan is equals to the total count of days in the month -->
+                <th colspan="6" class="th-months">December 2023</th>
+                <th colspan="4">Total</th>
+            </tr>
+            <tr class="dates">
+                <th>1</th>
+                <th>6</th>
+                <th>8</th>
+                <th>13</th>
+                <th>15</th>
+                <th>20</th>
+                <th>22</th>
+                <th>27</th>
+                <th>29</th>
+                <th>4</th>
+                <th>6</th>
+                <th>11</th>
+                <th>13</th>
+                <th>18</th>
+                <th>20</th>
+                <th>P</th>
+                <th>A</th>
+                <th>L</th>
+                <th>E</th>
             </tr>
         </thead>
-        <tbody>
+        <!-- 
+        ID Number	Name	                Nov-23								Dec-23					TOTAL			
+                                            1	6	8	13	15	20	22	27	29	4	6	11	13	18	20	P	A	L	E
+        -------------------------------------------------------------------------------------------------------------
+        202111227	Estores, Ivan James	    P	P	A	L	L	L	E	P	P	P	P	P	P	P	P	10	1	3	1
+        452415435	Garingo, Joshua Razzi	P	P	P	P	P	P	L	L	P	P	P	L	P	A	P	11	3	3	0
+        264314985	Geraldez, Jan Anthony	P	L	P	P	P	L	P	P	P	P	P	P	P	P	P	12	3	2	0
+        223457541	Hayag, Alexiss	        A	A	P	P	P	P	P	P	L	L	L	L	P	P	P	11	2	5	0
+        224563783	Taboada, Vene Lucille	P	P	P	P	P	P	P	P	P	L	L	P	P	L	P	10	0	3	0
+        324556843	Taladtad, Jelan Roy	    P	A	P	P	P	P	A	A	P	P	P	P	P	P	P	14	3	0	0
+         -->
+        <tbody >
             <tr>
                 <td>202112748</td>
                 <td class="fit">Garingo, Joshua Razzi</td>
@@ -89,9 +111,10 @@ $classId = isset($_GET['id']) ? $_GET['id'] : null;
                 <td>P</td>
                 <td>P</td>
                 <td>P</td>
-                <td>P</td>
-                <td>P</td>
-                <td class="fit">P=13 A=1 L=4 E=0</td>
+                <td>13</td>
+                <td>6</td>
+                <td>0</td>
+                <td>1</td>
             </tr>
             <tr>
                 <td>202143234</td>
@@ -106,14 +129,15 @@ $classId = isset($_GET['id']) ? $_GET['id'] : null;
                 <td>E</td>
                 <td>E</td>
                 <td>P</td>
-                <td>L</td>
                 <td>P</td>
                 <td>P</td>
                 <td>P</td>
                 <td>P</td>
                 <td>P</td>
-                <td>P</td>
-                <td class="fit">P=15 A=3 L=4 E=0</td>
+                <td>13</td>
+                <td>6</td>
+                <td>0</td>
+                <td>1</td>
             </tr>
             <tr>
                 <td class="fit">202143234</td>
@@ -132,10 +156,11 @@ $classId = isset($_GET['id']) ? $_GET['id'] : null;
                 <td>P</td>
                 <td>L</td>
                 <td>P</td>
-                <td>L</td>
                 <td>P</td>
-                <td>P</td>
-                <td class="fit">P=12 A=3 L=4 E=2</td>
+                <td>13</td>
+                <td class="status-warning">6</td>
+                <td>0</td>
+                <td>1</td>
             </tr>
         </tbody>
         </table>    
