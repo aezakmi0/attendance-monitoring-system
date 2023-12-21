2c4c946aa05216c1d84d1a144eae2f23edd960ea
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-22 01:57:59 +0800

    Update edit-student.php

--- edit-student.php
+++ edit-student.php
@@ -74,37 +74,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Student</title>
+    <title>Class Code</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
     <div class="container">
-        <form class="form-control mt-5 p-5" action="" method="post">
+        <form class="form-control bg-light mt-5 p-5" action="" method="post">
             <h1 class="text-center">Edit Student</h1>
             <div class="row">
                 <div class="col-md-2 mt-3">
                     <p class="label-text mb-1">STUDENT ID</p>
-                    <input type="text" name="id_number" class="form-control" value="<?php echo $studentData['ID_number']; ?>" required>
+                    <input type="text" name="id_number" class="form-control input-border" value="<?php echo $studentData['ID_number']; ?>" required>
                 </div>
                 <div class="col-md-4 mt-3">
                     <p class="label-text mb-1">FIRST NAME</p>
-                    <input type="text" name="first_name" class="form-control" value="<?php echo $studentData['first_name']; ?>" required>
+                    <input type="text" name="first_name" class="form-control input-border" value="<?php echo $studentData['first_name']; ?>" required>
                 </div>
                 <div class="col-md-4 mt-3">
                     <p class="label-text mb-1">LAST NAME</p>
-                    <input type="text" name="last_name" class="form-control" value="<?php echo $studentData['last_name']; ?>" required>
+                    <input type="text" name="last_name" class="form-control input-border" value="<?php echo $studentData['last_name']; ?>" required>
                 </div>
                 <div class="col-md-2">
                     <p class="label-text mb-1 mt-3 invisible">EDIT</p>
-                    <button class="btn btn-primary w-100" type="submit" value="Submit">Save Changes</button>
+                    <button class="btn input-border create-class-button w-100 type="submit" value="Submit">Save Changes</button>
                 </div>
             </div>
         </form>
         <div class="d-flex justify-content-center mt-4">
-            <a href="enroll-student.php?id=<?php echo $classID; ?>" type="button" class="btn btn-outline-secondary m-1">Back</a>
+            <a href="enroll-student.php?id=<?php echo $classID; ?>" type="button" class="btn btn-sm btn-outline-dark btn-black m-1">Back</a>
         </div>
     </div>
     <script src="assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>
