508a5cfcb92646384f306f30e620be905261b90c
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-22 11:20:20 +0800

    updated the calendar

--- calendar.js
+++ calendar.js
@@ -121,35 +121,35 @@ let currentMonth = { value: currentDate.getMonth() };
 let currentYear = { value: currentDate.getFullYear() };
 generateCalendar(currentMonth.value, currentYear.value);
 
-const todayShowTime = document.querySelector('.time-formate');
-const todayShowDate = document.querySelector('.date-formate');
+// const todayShowTime = document.querySelector('.time-formate');
+// const todayShowDate = document.querySelector('.date-formate');
 
-const currshowDate = new Date();
-const showCurrentDateOption = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  weekday: 'long',
-};
-const currentDateFormate = new Intl.DateTimeFormat(
-  'en-US',
-  showCurrentDateOption
-).format(currshowDate);
-todayShowDate.textContent = currentDateFormate;
-setInterval(() => {
-  const timer = new Date();
-  const option = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  };
-  const formateTimer = new Intl.DateTimeFormat('en-us', option).format(timer);
-  let time = `${`${timer.getHours()}`.padStart(
-      2,
-      '0'
-    )}:${`${timer.getMinutes()}`.padStart(
-      2,
-      '0'
-    )}: ${`${timer.getSeconds()}`.padStart(2, '0')}`;
-  todayShowTime.textContent = formateTimer;
-}, 1000);
+// const currshowDate = new Date();
+// const showCurrentDateOption = {
+//   year: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+//   weekday: 'long',
+// };
+// const currentDateFormate = new Intl.DateTimeFormat(
+//   'en-US',
+//   showCurrentDateOption
+// ).format(currshowDate);
+// todayShowDate.textContent = currentDateFormate;
+// setInterval(() => {
+//   const timer = new Date();
+//   const option = {
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     second: 'numeric',
+//   };
+//   const formateTimer = new Intl.DateTimeFormat('en-us', option).format(timer);
+//   let time = `${`${timer.getHours()}`.padStart(
+//       2,
+//       '0'
+//     )}:${`${timer.getMinutes()}`.padStart(
+//       2,
+//       '0'
+//     )}: ${`${timer.getSeconds()}`.padStart(2, '0')}`;
+//   todayShowTime.textContent = formateTimer;
+// }, 1000);
