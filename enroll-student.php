8c2958c2797f624ce945499f811b9064cebfa996
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2023-12-24 16:56:52 +0800

    updated the <title>

--- enroll-student.php
+++ enroll-student.php
@@ -47,7 +47,7 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class Code</title>
+    <title>Enroll Student</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
