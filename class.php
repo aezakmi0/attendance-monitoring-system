db44a35ce03ac947afa7ad994e0e361292951ad7
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2024-02-19 00:16:25 +0800

    Added back button

--- class.php
+++ class.php
@@ -118,8 +118,11 @@ if ($result->num_rows > 0) {
             <p class="class-name">Web Systems and Technology | 3:00PM-5:00PM TF</p> -->
             <!-- </div> -->
             <!-- <hr class="hr" /> -->
-        <div class="d-flex justify-content-between align-items-center mt-5">    
-            <?php include 'class-header.php'; ?>
+        <div class="d-flex justify-content-between align-items-center mt-5">  
+            <div class="d-flex">
+                <a href="index.html" class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m7.825 13l5.6 5.6L12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2z"/></svg></a>
+                <?php include 'class-header.php'; ?>
+            </div>  
             <div class="text-end">
                 <a href="edit-seatplan.php?id=<?php echo $classId; ?>" type="button" class="btn btn-sm btn-outline-dark btn-rounded btn-black">Edit Seatplan</a>
                 <a href="enroll-student.php?id=<?php echo $classId; ?>" type="button" class="btn btn-sm btn-outline-dark btn-rounded btn-black">Enroll Student</a>
