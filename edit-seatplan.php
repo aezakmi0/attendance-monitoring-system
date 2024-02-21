c0f4787dfee5a7dcc7698340e5f4274d66c311b9
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2024-02-21 20:00:01 +0800

    added the instructions

--- edit-seatplan.php
+++ edit-seatplan.php
@@ -94,7 +94,7 @@ $duplicateSeats = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <a href="class.php?id=<?php echo $classId; ?>" class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m7.825 13l5.6 5.6L12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2z"/></svg></a>
         <div>
             <h1 class="">Edit Seatplan</h1>
-            <!-- <h1 class="label-text text-center">Click seat to assign to student</h1>  -->
+            <h1 class="label-text text-center">Click seat to assign to student</h1> 
         </div>
        
     </div>
