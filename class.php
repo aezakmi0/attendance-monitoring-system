f40323232b9dbc1e2cde5fe59283dca1232f6477
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2024-02-24 18:54:25 +0800

    changed the instruction

--- class.php
+++ class.php
@@ -157,7 +157,7 @@ if ($result->num_rows > 0) {
                 <label><input type="radio" name="status" value="excused"> Excused</label>
 
             </div>
-            <p class="label-text-2">Click a seat to change the student's attendance status.</p>
+            <p class="label-text-2">Select an attendance status and click a seat to change the student's attendance status.</p>
             <span class="text-end">
                 <a href="#" id="markAllPresentBtn" class="btn btn-sm btn-rounded btn-green">Mark all as present</a>
                 <a href="#" type="button" class="btn btn-sm btn-rounded btn-green" onclick="reloadPage()"><svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17.65 6.35A7.958 7.958 0 0 0 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0 1 12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4z"/></svg></a>    
@@ -462,6 +462,7 @@ if ($result->num_rows > 0) {
                 <div class="seatplan-seat-content">
                     <p class="seatplan-lastname"></p>
                     <p class="seatplan-firstname"></p>
+                    <p class="seatplan-status">P</p>
                 </div>
             </div>
 
@@ -469,6 +470,7 @@ if ($result->num_rows > 0) {
                 <div class="seatplan-seat-content">
                     <p class="seatplan-lastname"></p>
                     <p class="seatplan-firstname"></p>
+                    <p class="seatplan-status">P</p>
                 </div>
             </div>
 
@@ -476,6 +478,7 @@ if ($result->num_rows > 0) {
                 <div class="seatplan-seat-content">
                     <p class="seatplan-lastname"></p>
                     <p class="seatplan-firstname"></p>
+                    <p class="seatplan-status">L</p>
                 </div>
             </div>
 
@@ -483,6 +486,7 @@ if ($result->num_rows > 0) {
                 <div class="seatplan-seat-content">
                     <p class="seatplan-lastname"></p>
                     <p class="seatplan-firstname"></p>
+                    <p class="seatplan-status">A</p>
                 </div>
             </div>
 
@@ -490,6 +494,7 @@ if ($result->num_rows > 0) {
                 <div class="seatplan-seat-content">
                     <p class="seatplan-lastname"></p>
                     <p class="seatplan-firstname"></p>
+                    <p class="seatplan-status">P</p>
                 </div>
             </div>
 
