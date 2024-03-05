aff7cdc85ffb458b3fab889c2db4d86bd3e0e31f
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2024-03-06 00:30:10 +0800

    added seatplan prompt

--- edit-seatplan.php
+++ edit-seatplan.php
@@ -93,12 +93,15 @@ $duplicateSeats = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <!-- Navbar -->
     <!-- <div id="navbar-container"></div> -->
 
-    <div class="container d-flex mt-5 mb-3 align-items-center" style="background-color: rgb(255, 255, 255);">
-        <a href="class.php?id=<?php echo $classId; ?>" class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m7.825 13l5.6 5.6L12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2z"/></svg></a>
-        <div>
-            <h1 class="">Edit Seatplan</h1>
-            <h1 class="label-text text-center">Click seat to assign to student</h1> 
-        </div>
+    <div class="container d-flex mt-5 mb-3 justify-content-between" style="background-color: rgb(255, 255, 255);">
+        <span class="d-flex">
+            <a href="class.php?id=<?php echo $classId; ?>" class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m7.825 13l5.6 5.6L12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2z"/></svg></a>
+            <div>
+                <h1 class="">Edit Seatplan</h1>
+                <h1 class="label-text text-center">Click seat to assign to student</h1> 
+            </div>
+        </span>    
+        <div class="align-self-end label-text-dark mt-1 text-base"><b>NOTE:</b> Changes to the seat plan are automatically saved.</div>
        
     </div>
     
