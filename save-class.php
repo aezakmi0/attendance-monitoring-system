e0599ec3e58495cb1769eb395693024aaf72d646
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2024-03-29 12:08:12 +0800

    prof can add class

--- save-class.php
+++ save-class.php
@@ -1,8 +1,10 @@
 <?php
+
+require_once 'includes/check_session.inc.php';
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
-    // $classId = $_POST['class_id'];
+    $user_ID = $_SESSION['user_id'];
     $class_code = $_POST["class_code"];
     $class_name = $_POST["class_name"];
     $room = $_POST["room"];
@@ -13,8 +15,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $selected_days = isset($_POST["day"]) && is_array($_POST["day"]) ? implode("", $_POST["day"]) : '';
 
     // SQL query to insert data into the database
-    $sql = "INSERT INTO tb_class (class_code, class_name, room, time_start, time_end, day)
-            VALUES ('$class_code', '$class_name', '$room', '$time_start', '$time_end', '$selected_days')";
+    $sql = "INSERT INTO tb_class (user_ID, class_code, class_name, room, time_start, time_end, day)
+            VALUES ('$user_ID', '$class_code', '$class_name', '$room', '$time_start', '$time_end', '$selected_days')";
 
     // Create connection (replace these values with your actual database credentials)
     $servername = "localhost";
