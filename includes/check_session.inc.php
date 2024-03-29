6af9ae28f420a6b6afa2f1ada90321a0a5a4f233
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2024-03-29 11:46:44 +0800

    log-in done

---
+++ includes/check_session.inc.php
@@ -0,0 +1,13 @@
+<?php
+// Start or resume the session
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to the login page if the user is not logged in
+    header("Location: welcome.php");
+    exit();
+}
+
+$user_ID = $_SESSION['user_id'];
+// You can now use $user_ID to perform actions related to the logged-in user
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'includes/check_session.inc.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
