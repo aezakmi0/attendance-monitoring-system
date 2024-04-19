102fac1457e58cfa8f6bbe68d1f3c3ac56597b76
aezakmi0 <129245648+aezakmi0@users.noreply.github.com>
2024-04-19 20:06:24 +0800

    Update config_session.inc.php

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -11,40 +11,40 @@ session_set_cookie_params([
     'httponly' => true
 ]);
 
+// Start the session
 session_start();
 
-// Regenerate session ID if user is logged in
-if (isset($_SESSION["user_id"])) {
-    if (isset($_SESSION["last_regeneration"])) {
-        regenerate_session_id_loggedin();
-    } else {
-        $_SESSION["last_regeneration"] = time();
-    }
-    
-    $interval = 60 * 30;
-    if (isset($_SESSION["last_regeneration"]) && time() - $_SESSION["last_regeneration"] >= $interval) {
-        regenerate_session_id_loggedin();
-    }
-} else {
-    // Regenerate session ID for non-logged-in users
-    if (isset($_SESSION["last_regeneration"])) {
-        regenerate_session_id();
-    } else {
-        $_SESSION["last_regeneration"] = time();
-    }
-    
-    $interval = 60 * 30;
-    if (isset($_SESSION["last_regeneration"]) && time() - $_SESSION["last_regeneration"] >= $interval) {
-        regenerate_session_id();
-    }
-}
-
-function regenerate_session_id(){
-    session_regenerate_id(true);
+// Initialize last_regeneration if not set
+if (!isset($_SESSION["last_regeneration"])) {
     $_SESSION["last_regeneration"] = time();
 }
 
+// Regenerate session ID if user is logged in
+if (isset($_SESSION["user_id"])) {
+    $interval = 60 * 30;
+    if (time() - $_SESSION["last_regeneration"] >= $interval) {
+        regenerate_session_id_loggedin();
+    }
+} else {
+    $interval = 60 * 30;
+    if (time() - $_SESSION["last_regeneration"] >= $interval) {
+        regenerate_session_id();
+    }
+}
+
 function regenerate_session_id_loggedin(){
-    session_regenerate_id(true);
+    session_write_close(); // Close the current session
+
+    $userId = $_SESSION["user_id"];
+    $newSessionId = session_create_id();
+    $sessionId = $newSessionId . "_" . $userId;
+    session_id($sessionId);
+    session_start(); // Start the new session
+    $_SESSION["last_regeneration"] = time();
+}
+
+function regenerate_session_id(){
+    session_write_close(); // Close the current session
+    session_start(); // Start a new session
     $_SESSION["last_regeneration"] = time();
 }
